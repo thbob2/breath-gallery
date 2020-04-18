d133421d82363e33fe4f62534a46bd80e8ab97af
Chouaib <chouaib.dot@hotmail.com>
2020-04-18 19:33:37 +0100

    done

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Animate Smooth Scroll
+$('#view-work').on('click', function() {
+    const images = $('#images').position().top;
+    $('html, body').animate(
+        {
+            scrollTop: images
+        },900
+    );
+});
